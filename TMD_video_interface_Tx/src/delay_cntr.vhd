eec5d863398aa2b1ecff3cc3de3a7c198f637637
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-06-22 15:59:13 -0700

    made rst async, removed generics

--- TMD_video_interface_Tx/src/delay_cntr.vhd
+++ TMD_video_interface_Tx/src/delay_cntr.vhd
@@ -8,7 +8,7 @@
 -- Project Name: TMDS Video Interface Tx
 -- Target Devices: 
 -- Tool Versions: 
--- Description: Counts a defined number of clock cycles in the "pclk" domain. This counter
+-- Description: Counts 9 clk cycles in the "pclk" domain. This counter
 --              is used to delay the reset release for a certain amount of time. 
 -- 
 -- Dependencies: 
@@ -25,35 +25,31 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity delay_cntr is
-    generic (
-      g_delay_cycles : integer := 10;  -- default 10 pclk cycle delay (400 ns)
-      g_cntr_width : integer := 4  -- 4 bits encodes >= 10 values
-      );
     port ( 
-      i_en : in std_logic;  -- counter enable
+      i_en : in std_logic;
       i_pclk : in std_logic; 
-      o_delay_cntr : out std_logic_vector(g_cntr_width-1 downto 0)
+      o_delay_cntr : out std_logic_vector(2 downto 0)
       );
 end delay_cntr;
 
 architecture rtl of delay_cntr is
 
-  signal r_delay_cntr : unsigned(g_cntr_width-1 downto 0) := (others => '0');
+  signal r_delay_cntr : unsigned(2 downto 0) := (others => '0');
  
 begin
 
-  CLK_COUNT_PROC : process(i_pclk)
-  begin 
-    if rising_edge(i_pclk) then 
-      if i_en = '1' then 
-        if r_delay_cntr < g_delay_cycles - 2 then  -- why "g_delay_cycles - 2" in the counter? Because state machine in sync_rst_release.vhd has latency of 1 clk cycle.
+  CLK_COUNT_PROC : process(i_pclk, i_en)
+  begin
+    if i_en = '0' then 
+      r_delay_cntr <= (others => '0');
+    else 
+      if rising_edge(i_pclk) then  
+        if r_delay_cntr < "101" then            -- why "5" in the counter and not 9? Upstream latency.
           r_delay_cntr <= r_delay_cntr + 1;
         else 
           r_delay_cntr <= (others => '0');
-        end if;
-      else 
-        r_delay_cntr <= (others => '0');
-      end if;   
+        end if; 
+      end if;
     end if;
   end process;
   
