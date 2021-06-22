076c35f42d91a2138c7abe1f8386b8b17450fa24
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-06-22 16:00:30 -0700

    removed generics, changed counter enable routing

--- TMD_video_interface_Tx/src/sync_rst_release.vhd
+++ TMD_video_interface_Tx/src/sync_rst_release.vhd
@@ -9,8 +9,8 @@
 -- Target Devices: 7-Series FPGAs and SoCs
 -- Tool Versions: 
 -- Description: Uses the "locked" output from the PLL as a reset. By default, the "locked" output is not synchronous
---              with the output clocks of the PLL. This circuit takes in the "locked" signal fromm the PLL,
---              and synchronously de-asserts (after a defined number of clock cycles) a "locked_sync/reset" signal 
+--              with the output clocks of the PLL. This circuit takes in the "locked" signal from the PLL when is is active,
+--              and synchronously de-asserts (after 10 FULL clock cycles) a "locked_sync/reset" signal 
 --              with respect to the slower output clk of the PLL. The output reset can still be asynchronously asserted.
 --
 --             (Adapted from schematic by vivado forum user: stripline, with input from EEVblog users -- links below)
@@ -26,15 +26,15 @@
 ----------------------------------------------------------------------------------
 
 
-
 library ieee;
-use ieee.std_logic_1164.ALL;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 library UNISIM;
 use UNISIM.VComponents.all;
 
 
 entity sync_rst_release is
-  Port ( 
+  port ( 
     i_locked : in std_logic;
     i_rst : in std_logic;
     i_pclk : in std_logic;
@@ -46,58 +46,57 @@ architecture rtl of sync_rst_release is
 
   signal w_DFF1_to_DFF2 : std_logic;
   signal w_locked_sync : std_logic;
-  signal w_delay_cntr : std_logic_vector(3 downto 0);
-  signal r_rst_sync : std_logic := '0';
+  signal w_delay_cntr : std_logic_vector(2 downto 0);
+  signal r_rst_sync : std_logic := '1';
+  signal r_delay_cntr_en : std_logic := '0';
   
   type t_state is (WAITING_STATE, DELAY_STATE, RELEASE_STATE);
   signal STATE : t_state;
  
 begin
-
-   FDRE_1 : FDRE
+   
+   FDCE_0 : FDCE
    generic map (
       INIT => '0') 
    port map (
-      Q => w_DFF1_to_DFF2,      
-      C => i_pclk,      
-      CE => '1',    
-      R => '0',      
-      D => i_locked       
+      Q => w_DFF1_to_DFF2,     
+      C => i_pclk,    
+      CE => '1', 
+      CLR => i_rst, 
+      D => i_locked     
    );
-    
-  FDRE_2 : FDRE
+  
+   FDCE_1 : FDCE
    generic map (
       INIT => '0') 
    port map (
-      Q => w_locked_sync,      
-      C => i_pclk,      
-      CE => '1',    
-      R => '0',      
-      D => w_DFF1_to_DFF2      
+      Q => w_locked_sync,     
+      C => i_pclk,    
+      CE => '1', 
+      CLR => i_rst, 
+      D => w_DFF1_to_DFF2     
    );
     
-  delay_cntr_1 : entity work.delay_cntr(rtl)
-  generic map(
-    g_delay_cycles => 10,
-    g_cntr_width => 4
-    )
+  delay_cntr_0 : entity work.delay_cntr(rtl)
   port map(
-    i_en => w_locked_sync,
+    i_en => r_delay_cntr_en,
     i_pclk => i_pclk,
     o_delay_cntr => w_delay_cntr
     );
     
     
-  STATE_MACHINE_PROC : process(i_pclk)
+  STATE_MACHINE_PROC : process(i_pclk, i_rst)
   begin 
-    if rising_edge(i_pclk) then 
-      if i_rst = '1' then 
+    if i_rst = '1' then 
         r_rst_sync <= '1';
+        r_delay_cntr_en <= '0';
         STATE <= WAITING_STATE;  -- default is waiting for "locked_sync"
-      else 
+    else 
+      if rising_edge(i_pclk) then  
         case STATE is 
           when WAITING_STATE =>  -- WAITING_STATE waits for the "locked_sync" signal to go high
             r_rst_sync <= '1';
+            r_delay_cntr_en <= '0';
             if w_locked_sync = '1' then 
               STATE <= DELAY_STATE;
             else 
@@ -105,19 +104,21 @@ begin
             end if;
           when DELAY_STATE =>  -- DELAY_STATE sees the high "locked_sync" signal, and enables the delay_cntr for the reset
             r_rst_sync <= '1';
-            if w_delay_cntr = "11" then 
+            r_delay_cntr_en <= '1';
+            if w_delay_cntr = "101" then  -- 5 and not 9 because 2 clk cycles latency asscoiated with double flip flops, 1 from state machine, and 1 from counter in delay_cntr.vhdl
               STATE <= RELEASE_STATE;
             else 
               STATE <= DELAY_STATE;
             end if;
           when RELEASE_STATE =>  -- after the delay, the RELEASE_STATE releases the output reset (unless input reset is active)
             r_rst_sync <= '0';
+            r_delay_cntr_en <= '0';
         end case;
       end if;
     end if;  
   end process;
   
-  o_rst_sync <= r_rst_sync and w_locked_sync;  -- AND gate insures asynchronous assertion along with synchronous de-assertion
+  o_rst_sync <= r_rst_sync; 
 
 end rtl;
 
