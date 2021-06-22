410e06acc3da72d82f5ccf420f59204513246f4e
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-06-22 16:01:49 -0700

    added port for 50 MHz clk

--- TMD_video_interface_Tx/sim/clk_gen_tb.vhd
+++ TMD_video_interface_Tx/sim/clk_gen_tb.vhd
@@ -8,9 +8,9 @@
 -- Project Name: TMDS Video Interface Tx
 -- Target Devices: 7-Series FPGAs and SoCs
 -- Tool Versions: 
--- Description: PLL instantiaion that generates a 25 MHz pixel clk (o_pclk), a 250 MHz clk (o_pclkx10),
---              and a "locked" output that is used as a reset in other logic
--- 
+-- Description: PLL instantiaion that generates a 25 MHz pixel clk (o_pclk), a 50 MHz clk (o_pclk_x2), 
+--              a 250 MHz clk (o_pclkx10), and a "locked" output that is used as a reset in other logic
+--  
 -- Dependencies: Input clk = 125 MHz
 -- 
 -- Revision:
@@ -20,8 +20,8 @@
 ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 
 entity clk_gen_tb is
@@ -29,22 +29,24 @@ end clk_gen_tb;
 
 architecture sim of clk_gen_tb is
 
-  constant clk_hz : INTEGER := 125e6;
-  constant clk_period : TIME := 1 sec / clk_hz;
+  constant clk_hz : integer := 125e6;
+  constant clk_period : time := 1 sec / clk_hz;
   
-  signal i_clk : STD_LOGIC := '1';
-  signal i_rst_n : STD_LOGIC := '0';
-  signal o_pclk : STD_LOGIC;
-  signal o_pclk_x10 : STD_LOGIC;
-  signal o_locked : STD_LOGIC;
+  signal i_clk : std_logic := '1';
+  signal i_rst : std_logic := '1';
+  signal o_pclk : std_logic;
+  signal o_pclk_x2 : std_logic;
+  signal o_pclk_x10 : std_logic;
+  signal o_locked : std_logic;
 
 begin
 
   DUT : entity work.clk_gen(rtl)
   port map(
     i_clk => i_clk,
-    i_rst_n => i_rst_n,
+    i_rst => i_rst,
     o_pclk => o_pclk,
+    o_pclk_x2 => o_pclk_x2,
     o_pclk_x10 => o_pclk_x10,
     o_locked => o_locked
     );
@@ -58,9 +60,9 @@ begin
   STIM_PROC : process 
   begin 
     wait for 50 ns;
-    i_rst_n <= '1';
+    i_rst <= '0';
     wait for 1000 ns;
-    i_rst_n <= '0';
+    i_rst <= '1';
     wait for 50 ns;
     wait;
   end process;
