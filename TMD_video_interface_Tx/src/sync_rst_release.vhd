98f32c16ad1d2fa1cd9a4f2f7b022c2c0fdabca3
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-06-21 21:52:47 -0700

    changed reset polarity
    also added some combination logic so the output reset can still be asserted asynchronously

--- TMD_video_interface_Tx/src/sync_rst_release.vhd
+++ TMD_video_interface_Tx/src/sync_rst_release.vhd
@@ -11,7 +11,7 @@
 -- Description: Uses the "locked" output from the PLL as a reset. By default, the "locked" output is not synchronous
 --              with the output clocks of the PLL. This circuit takes in the "locked" signal fromm the PLL,
 --              and synchronously de-asserts (after a defined number of clock cycles) a "locked_sync/reset" signal 
---              with respect to the slower output clk of the PLL. 
+--              with respect to the slower output clk of the PLL. The output reset can still be asynchronously asserted.
 --
 --             (Adapted from schematic by vivado forum user: stripline, with input from EEVblog users -- links below)
 --             schematic link: https://forums.xilinx.com/t5/Virtex-Family-FPGAs-Archived/How-to-synchronize-LOCKED-output-of-PLL-for-use-as-reset/td-p/561924
@@ -33,21 +33,21 @@ library UNISIM;
 use UNISIM.VComponents.all;
 
 
-entity synch_rst_release is
+entity sync_rst_release is
   Port ( 
     i_locked : in std_logic;
-    i_rst_n : in std_logic;
+    i_rst : in std_logic;
     i_pclk : in std_logic;
-    o_rst_n : out std_logic
+    o_rst_sync : out std_logic
   );
 end synch_rst_release;
 
-architecture rtl of synch_rst_release is
+architecture rtl of sync_rst_release is
 
   signal w_DFF1_to_DFF2 : std_logic;
   signal w_locked_sync : std_logic;
   signal w_delay_cntr : std_logic_vector(3 downto 0);
-  signal r_rst_n : std_logic := '0';
+  signal r_rst_sync : std_logic := '0';
   
   type t_state is (WAITING_STATE, DELAY_STATE, RELEASE_STATE);
   signal STATE : t_state;
@@ -91,32 +91,33 @@ begin
   STATE_MACHINE_PROC : process(i_pclk)
   begin 
     if rising_edge(i_pclk) then 
-      if i_rst_n = '0' then 
-        r_rst_n <= '0';
+      if i_rst = '1' then 
+        r_rst_sync <= '1';
         STATE <= WAITING_STATE;  -- default is waiting for "locked_sync"
       else 
         case STATE is 
           when WAITING_STATE =>  -- WAITING_STATE waits for the "locked_sync" signal to go high
-            r_rst_n <= '0';
+            r_rst_sync <= '1';
             if w_locked_sync = '1' then 
               STATE <= DELAY_STATE;
             else 
               STATE <= WAITING_STATE;
             end if;
           when DELAY_STATE =>  -- DELAY_STATE sees the high "locked_sync" signal, and enables the delay_cntr for the reset
-            r_rst_n <= '0';
+            r_rst_sync <= '1';
             if w_delay_cntr = "11" then 
               STATE <= RELEASE_STATE;
             else 
               STATE <= DELAY_STATE;
             end if;
           when RELEASE_STATE =>  -- after the delay, the RELEASE_STATE releases the output reset (unless input reset is active)
-            r_rst_n <= '1';
+            r_rst_sync <= '0';
         end case;
       end if;
     end if;  
   end process;
   
-  o_rst_n <= r_rst_n;
+  o_rst_sync <= r_rst_sync and w_locked_sync;  -- AND gate insures asynchronous assertion along with synchronous de-assertion
 
 end rtl;
+
