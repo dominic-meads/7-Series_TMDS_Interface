6a27ac73be91dc0d95800012c2bb0b49c0fb5447
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-06-28 15:11:38 -0700

    Update clk_gen_tb.vhd

--- TMD_video_interface_Tx/prototype/PLL_test/clk_gen_tb.vhd
+++ TMD_video_interface_Tx/prototype/PLL_test/clk_gen_tb.vhd
@@ -2,15 +2,16 @@
 -- Company: 
 -- Engineer: Dominic Meads
 -- 
--- Create Date: 05/01/2021 10:40:47 PM
+-- Create Date: 05/08/2021 04:36:09 PM
 -- Design Name: 
 -- Module Name: clk_gen_tb - sim
--- Project Name: 
--- Target Devices: 
+-- Project Name: TMDS Video Interface Tx
+-- Target Devices: 7-Series FPGAs and SoCs
 -- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
+-- Description: PLL instantiaion that generates a 25 MHz pixel clk (o_pclk), a 50 MHz clk (o_pclk_x2), 
+--              a 250 MHz clk (o_pclkx10), and a "locked" output that is used as a reset in other logic
+--  
+-- Dependencies: Input clk = 125 MHz
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
@@ -19,10 +20,9 @@
 ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-library UNISIM;
-use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
+
 
 entity clk_gen_tb is
 end clk_gen_tb;
@@ -35,6 +35,7 @@ architecture sim of clk_gen_tb is
   signal i_clk : std_logic := '1';
   signal i_rst : std_logic := '1';
   signal o_pclk : std_logic;
+  signal o_pclk_x2 : std_logic;
   signal o_pclk_x10 : std_logic;
   signal o_locked : std_logic;
 
@@ -45,6 +46,7 @@ begin
     i_clk => i_clk,
     i_rst => i_rst,
     o_pclk => o_pclk,
+    o_pclk_x2 => o_pclk_x2,
     o_pclk_x10 => o_pclk_x10,
     o_locked => o_locked
     );
@@ -59,8 +61,11 @@ begin
   begin 
     wait for 50 ns;
     i_rst <= '0';
-    wait for 250 ns;
+    wait for 1000 ns;
+    i_rst <= '1';
+    wait for 50 ns;
     wait;
   end process;
   
 end sim;
+© 2021 GitHub, Inc.
